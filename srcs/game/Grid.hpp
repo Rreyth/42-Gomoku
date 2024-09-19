41599619d48e60043612075cb0fb6d31bff9eb8e
Gugus <gugus.deroubaix@gmail.com>
2024-09-19 18:49:31 +0200

    remove useless comments

--- srcs/game/Grid.hpp
+++ srcs/game/Grid.hpp
@@ -46,18 +46,6 @@ public:
 	std::vector<sf::Vector2i>	getLegalMoves(Player *player, Player *opponent);
 	std::vector<sf::Vector2i>	getInterestingMoves(Player *player, Player *opponent);
 	std::vector<Move>			getInterestingMovesSorted(Evaluation *evaluator, Player *player, Player *opponent, bool reverse, Tracker *tracker);
-	// bool						checkInterestingMove(int x, int y);
-	// std::vector<sf::Vector2i>	getInterestingMovesSorted(Player *player, Player *opponent, Evaluation *evaluator, bool reverse, Tracker *tracker);
-	// void						computeInterestingMovesSorted(Player *player, Player *opponent, Evaluation *evaluator, Tracker *tracker);
-	// void						updateInterestingMovesSorted(Player *player, Player *opponent, Evaluation *evaluator, Tracker *tracker, sf::Vector2i *move);
-	// std::vector<sf::Vector2i>	getInterestingMovesSortedSaved(inter_type plType);
-	// void						saveInterestingMovesSorted(std::vector<sf::Vector2i> *movesLeft, std::vector<sf::Vector2i> *movesRight);
-	// void						loadInterestingMovesSorted(std::vector<sf::Vector2i> *movesLeft, std::vector<sf::Vector2i> *movesRight);
-	// void						resetGridByBoardState(std::string boardState, Tracker *tracker);
-	// void						saveBbox(sf::Vector2i *bboxUL, sf::Vector2i *bboxDR);
-	// void						loadBbox(sf::Vector2i *bboxUL, sf::Vector2i *bboxDR);
-	// void						saveWinPos(std::vector<sf::Vector2i> *leftWinPos, std::vector<sf::Vector2i> *rightWinPos);
-	// void						loadWinPos(std::vector<sf::Vector2i> *leftWinPos, std::vector<sf::Vector2i> *rightWinPos);
 
 private:
 	int											x, y, w, h, previewX, previewY, boardHistoryId;
@@ -68,12 +56,6 @@ private:
 	BitBoard									bitboardL, bitboardR;
 	std::vector<std::pair<BitBoard, BitBoard>>	boardHistory;
 
-	// intersection				gridState[GRID_NB_INTER];
-	// sf::Vector2i				dirs[8], bboxUL, bboxDR;
-	// std::vector<sf::Vector2i>	leftWinPos, rightWinPos, interestingMovesLeft, interestingMovesRight;
-	// std::vector<std::string>	boardStates;
-	// std::string					currentBoardState, currentBoardStateOpti;
-
 	bool	checkWinByAlign(Player *player, Player *opponent, BitBoard *plBitboard, BitBoard *opBitboard);
 	bool	validateWin(Player *player, Player *opponent, BitBoard *plBitBoard, BitBoard *opBitBoard, bitboardAxis bbAxis, int x, int y);
 	int		makeCapture(sf::Vector2i *move, BitBoard *plBitBoard, BitBoard *opBitBoard);
@@ -81,7 +63,6 @@ private:
 	bool	checkProRule(int x, int y, inter_type interPlayer, int nbMoves);
 	void	applyHistoryToGrid(void);
 
-	// void			insertMoves(std::vector<sf::Vector2i> &moves, sf::Vector2i *move, bool reverse, Evaluation *evaluator, Player *player, Player *opponent);
 };
 
 #endif
