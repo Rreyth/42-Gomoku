2e8ad5f10ddf1c769ee94d5ad059e95eafcbb509
agtdbx <gugus@PC-Auguste>
2024-10-09 16:43:09 +0200

    recreate Node class

--- srcs/AI/AI_hard.cpp
+++ srcs/AI/AI_hard.cpp
@@ -1,20 +1,12 @@
 #include <AI/AI.hpp>
 #include <utils/Functions.hpp>
 
-static Move	mtdf(
+static Move	negaMax(
 				std::unordered_map<int, std::vector<Move>> *memoryMoves,
 				std::unordered_map<int, int> *memoryEval,
 				Grid *grid, PlayerInfo *player, PlayerInfo *opponent,
-				Evaluation *evaluator,
-				int depth, Move firstGuess,
-				Tracker *tracker);
-static Move	pvs(
-				std::unordered_map<int, std::vector<Move>> *memoryMoves,
-				std::unordered_map<int, int> *memoryEval,
-				Grid *grid, PlayerInfo *player, PlayerInfo *opponent,
-				Evaluation *evaluator, bool maximizingEval,
-				int alpha, int beta, int depth,
-				Tracker *tracker);
+				Evaluation *evaluator, bool playerTurn, int alpha, int beta,
+				int depth, Tracker *tracker);
 static void	makeMove(
 				Grid *grid, sf::Vector2i *move, int nbMove, int depth,
 				PlayerInfo *player, PlayerInfo *opponent, bool playerTurn,
@@ -35,69 +27,18 @@ sf::Vector2i	getHardMove(
 					Grid *grid, PlayerInfo *player, PlayerInfo *opponent,
 					Evaluation *evaluator, Tracker *tracker)
 {
-	Move				bestMove;
-	std::vector<Move>	moves;
+	Move	bestMove;
 
-	// bestMove = pvs(memoryMoves, memoryEval, grid,
-	// 				player, opponent, evaluator,
-	// 				true, -1000000001, 1000000001,
-	// 				AI_HARD_DEPTH, tracker);
-
-	moves = grid->getInterestingMovesSorted(
-					evaluator, player, opponent, true, tracker);
-	if (moves.size() == 0)
-	{
-		if (player->nbMove + opponent->nbMove == 0)
-			return (sf::Vector2i(GRID_W_INTER / 2, GRID_W_INTER / 2));
-		return (sf::Vector2i(-1, -1));
-	}
-
-	bestMove = moves[0];
-	bestMove = mtdf(
-				memoryMoves, memoryEval, grid, player, opponent,
-				evaluator, AI_HARD_DEPTH, bestMove, tracker);
+	bestMove = negaMax(memoryMoves, memoryEval, grid,
+					player, opponent, evaluator,
+					true, -1000000001, 1000000001,
+					AI_HARD_DEPTH, tracker);
 
 	return (bestMove.pos);
 }
 
 
-static Move	mtdf(
-				std::unordered_map<int, std::vector<Move>> *memoryMoves,
-				std::unordered_map<int, int> *memoryEval,
-				Grid *grid, PlayerInfo *player, PlayerInfo *opponent,
-				Evaluation *evaluator,
-				int depth, Move firstGuess, Tracker *tracker)
-{
-	Move	bestMove;
-	int		upperBound, lowerBound, beta;
-
-	bestMove = firstGuess;
-	upperBound = 1000000001;
-	lowerBound = -1000000001;
-
-	while (lowerBound < upperBound)
-	{
-		// Set alpha beta
-		if (bestMove.eval == lowerBound)
-			beta = bestMove.eval + 1;
-		else
-			beta = bestMove.eval;
-
-		bestMove = pvs(
-					memoryMoves, memoryEval, grid, player, opponent,
-					evaluator, true, beta - 1, beta, depth, tracker);
-
-		if (bestMove.eval < beta)
-			upperBound = bestMove.eval;
-		else
-			lowerBound = bestMove.eval;
-	}
-
-	return (bestMove);
-}
-
-
-static Move	pvs(
+static Move	negaMax(
 				std::unordered_map<int, std::vector<Move>> *memoryMoves,
 				std::unordered_map<int, int> *memoryEval,
 				Grid *grid, PlayerInfo *player, PlayerInfo *opponent,
@@ -218,7 +159,7 @@ static Move	pvs(
 
 	bestMove.eval = -1000000001;
 	bestMove.pos = sf::Vector2i(-1, -1);
-	for (int i = 0; i < moves.size() && i < AI_HARD_LIMIT; i++)
+	for (int i = 0; i < moves.size() && i < AI_MEDIUM_LIMIT; i++)
 	{
 		// Make the move
 		makeMove(
@@ -234,38 +175,14 @@ static Move	pvs(
 		// A killer move is the end of the game, so don't contine recursion
 		if (tmpMove.eval == 0)
 		{
-			// For first child, do a classic recursion
-			if (i == 0)
-			{
-				// Call pvs to next player reply score
-				tmpMove = pvs(
-							memoryMoves, memoryEval,
-							grid, player, opponent, evaluator, !playerTurn,
-							-beta, -alpha, depth - 1, tracker);
-				// Reverse evaluation because a good opponent move is
-				// a bad move for player
-				tmpMove.eval = -tmpMove.eval;
-			}
-			else
-			{
-				// Do a first 0-window search
-				tmpMove = pvs(
-							memoryMoves, memoryEval,
-							grid, player, opponent, evaluator, !playerTurn,
-							-alpha - 1, -alpha, depth - 1, tracker);
-				tmpMove.eval = -tmpMove.eval;
-
-				// If the 0-window search
-				if (alpha < tmpMove.eval && tmpMove.eval < beta)
-				{
-					// Do a classic one
-					tmpMove = pvs(
-								memoryMoves, memoryEval,
-								grid, player, opponent, evaluator, !playerTurn,
-								-beta, -alpha, depth - 1, tracker);
-					tmpMove.eval = -tmpMove.eval;
-				}
-			}
+			// Call negaMax to next player reply score
+			tmpMove = negaMax(
+						memoryMoves, memoryEval,
+						grid, player, opponent, evaluator, !playerTurn,
+						-beta, -alpha, depth - 1, tracker);
+			// Reverse evaluation because a good opponent move is
+			// a bad move for player
+			tmpMove.eval = -tmpMove.eval;
 		}
 
 		// Reverse the move
@@ -437,3 +354,203 @@ static int	checkKillerMove(
 
 	return (score);
 }
+
+
+
+/*
+static Move	pvs(
+				std::unordered_map<int, std::vector<Move>> *memoryMoves,
+				std::unordered_map<int, int> *memoryEval,
+				Grid *grid, PlayerInfo *player, PlayerInfo *opponent,
+				Evaluation *evaluator, bool playerTurn, int alpha, int beta,
+				int depth, Tracker *tracker)
+{
+	int					nbMove, plSaveNbCapture, opSaveNbCapture, hash;
+	std::vector<Move>	moves;
+	Move				bestMove, tmpMove;
+	BitBoard			saveBitboard, *plBitboard, *opBitboard;
+	BboxManager			saveBboxManager;
+	BoardState			boardState;
+	std::unordered_map<int, int>::const_iterator				evalFind;
+	std::unordered_map<int, std::vector<Move>>::const_iterator	movesFind;
+
+	// TODO : REMOVE
+	std::clock_t	start;
+	int				diff;
+
+	plBitboard = grid->getBitBoard(player->interType);
+	opBitboard = grid->getBitBoard(opponent->interType);
+	// Compute hash of the current grid
+	boardState = BoardState(plBitboard, opBitboard);
+	hash = boardState.getHash();
+
+	if (depth <= 0)
+	{
+		start = std::clock();
+		tmpMove.pos = sf::Vector2i(-1, -1);
+
+		// Check in memory if we have already compute the evaluation
+		evalFind = memoryEval->find(hash);
+
+		diff = ((double)(std::clock() - start) / CLOCKS_PER_SEC) * 1000000;
+		tracker->evaluationTime += diff;
+		tracker->nbEvaluations++;
+
+		// If the evaluate have already be compute, get it's value from saved one
+		if (evalFind != memoryEval->end())
+		{
+			tmpMove.eval = evalFind->second;
+			tracker->nbMemoryEval++;
+		}
+		// Else compute the evaluation and put it in memory
+		else
+		{
+			start = std::clock();
+
+			// If this is player turn, evaluate position from it's view point
+			if (playerTurn)
+				tmpMove.eval = evaluator->evaluateGrid(
+											plBitboard,
+											opBitboard,
+											player->nbCapture, opponent->nbCapture);
+			// If this is opponent turn, evaluate position from it's view point
+			else
+				tmpMove.eval = evaluator->evaluateGrid(
+											opBitboard,
+											plBitboard,
+											opponent->nbCapture, player->nbCapture);
+
+			// Put the result in memory to avoid to recompute the evaluation for this board
+			memoryEval->insert(std::pair<int, int>(hash, tmpMove.eval));
+
+			diff = ((double)(std::clock() - start) / CLOCKS_PER_SEC) * 1000000;
+			tracker->computeEvalTime += diff;
+			tracker->nbComputeEval++;
+
+			tracker->evaluationTime += diff;
+		}
+
+		return (tmpMove);
+	}
+
+	// Compute variables
+	nbMove = player->nbMove + opponent->nbMove;
+	plSaveNbCapture = player->nbCapture;
+	opSaveNbCapture = opponent->nbCapture;
+	// If this is player turn, save only oppenent bitboard for capture case
+	if (playerTurn)
+		saveBitboard = *grid->getBitBoard(opponent->interType);
+	else
+		saveBitboard = *grid->getBitBoard(player->interType);
+	if (depth > 1)
+		saveBboxManager = *grid->getBboxManager();
+
+	// Get possible moves
+	movesFind = memoryMoves->find(hash);
+
+	// If moves is in memory, get moves from it
+	if (movesFind != memoryMoves->end())
+	{
+		moves = movesFind->second;
+	}
+	// Else compute moves and put them in memory
+	else
+	{
+		if (playerTurn)
+			moves = grid->getInterestingMovesSorted(evaluator, player, opponent, true, tracker);
+		else
+			moves = grid->getInterestingMovesSorted(evaluator, opponent, player, true, tracker);
+
+		memoryMoves->insert(std::pair<int, std::vector<Move>>(hash, moves));
+	}
+
+	// If there is no move, 2 case possible
+	if (moves.size() == 0)
+	{
+		tmpMove.eval = 0;
+		// This is the first move, play at center
+		if (nbMove == 0)
+			tmpMove.pos = sf::Vector2i(GRID_W_INTER / 2, GRID_W_INTER / 2);
+		// This is end game, return an error
+		else
+			tmpMove.pos = sf::Vector2i(-1, -1);
+		return (tmpMove);
+	}
+
+	bestMove.eval = -1000000001;
+	bestMove.pos = sf::Vector2i(-1, -1);
+	for (int i = 0; i < moves.size() && i < AI_HARD_LIMIT; i++)
+	{
+		// Make the move
+		makeMove(
+			grid, &moves[i].pos, nbMove, depth, player, opponent, playerTurn,
+			tracker);
+
+		// Check if move is a killer move
+		tmpMove.eval = checkKillerMove(
+							grid, playerTurn, moves[i].eval, player, opponent,
+							tracker);
+
+		// Check if this move isn't a killer one. In this case, the eval is 0
+		// A killer move is the end of the game, so don't contine recursion
+		if (tmpMove.eval == 0)
+		{
+			// For first child, do a classic recursion
+			if (i == 0)
+			{
+				// Call pvs to next player reply score
+				tmpMove = pvs(
+							memoryMoves, memoryEval,
+							grid, player, opponent, evaluator, !playerTurn,
+							-beta, -alpha, depth - 1, tracker);
+				// Reverse evaluation because a good opponent move is
+				// a bad move for player
+				tmpMove.eval = -tmpMove.eval;
+			}
+			else
+			{
+				// Do a first 0-window search
+				tmpMove = pvs(
+							memoryMoves, memoryEval,
+							grid, player, opponent, evaluator, !playerTurn,
+							-alpha - 1, -alpha, depth - 1, tracker);
+				tmpMove.eval = -tmpMove.eval;
+
+				// If the 0-window search fail
+				if (alpha < tmpMove.eval && tmpMove.eval < beta)
+				{
+					// Do a classic one
+					tmpMove = pvs(
+								memoryMoves, memoryEval,
+								grid, player, opponent, evaluator, !playerTurn,
+								-beta, -alpha, depth - 1, tracker);
+					tmpMove.eval = -tmpMove.eval;
+				}
+			}
+		}
+
+		// Reverse the move
+		unmakeMove(
+			grid, &moves[i].pos, nbMove, depth, player, opponent, playerTurn,
+			&saveBitboard, &saveBboxManager,
+			plSaveNbCapture, opSaveNbCapture);
+
+		// Keep the best move encounter so far
+		if (tmpMove.eval > bestMove.eval)
+		{
+			bestMove.eval = tmpMove.eval;
+			bestMove.pos = moves[i].pos;
+		}
+
+		// Update alpha
+		if (tmpMove.eval > alpha)
+			alpha = tmpMove.eval;
+
+		// If the move is too good, opponent will avoir this move
+		if (alpha >= beta)
+			break;
+	}
+
+	return (bestMove);
+}
+*/
--- srcs/game/Node.hpp
+++ srcs/game/Node.hpp
@@ -13,42 +13,16 @@ class Node
 {
 public:
 	Node(void);
-	Node(
-		Grid *grid, PlayerInfo *player, PlayerInfo *opponent,
-		bool playerTurn, Move *moveFrom);
 	Node(const Node &obj);
 	~Node();
 
-	Move	*getMove(void);
-	int		getNbMove(void);
-	int		getKillerScore(void);
-
 	Node	&operator=(const Node &obj);
-
-	// Update node methods
-	Node	*getActualNode(Grid *grid);
-	Node	*getNextNode(sf::Vector2i *move);
-
-	// Recursion methods
-	int					getEvaluation(Evaluation *evaluator, Tracker *tracker);
-	std::vector<Node>	*getChildren(Evaluation *evaluator, Tracker *tracker);
-	void				applyMove(int depth, Tracker *tracker);
-	bool				isKillerMove(void);
 
 	// TODO: REMOVE
 	void	printGrid(void);
 
 private:
-	int					evaluation, hash, killerMoveScore;
-	bool				playerTurn,
-						childrenCompute, evaluationCompute,
-						moveCompute, bboxManagerUpdate,
-						killerMoveCompute, killerMoveResult;
-	Grid				grid;
-	BitBoard			*plBitboard, *opBitboard;
-	PlayerInfo			player, opponent;
-	Move				lastMove;
-	std::vector<Node>	children;
+
 };
 
 #endif
